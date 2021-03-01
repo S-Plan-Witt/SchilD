dbf8e8e1518776f4e972acd99efd7c297bb19e2f
Nils Witt <nils@nils-witt.de>
2021-03-01 18:27:16 +0100

    added gui env

--- src/main/java/de/nils_witt/splan/Main.java
+++ src/main/java/de/nils_witt/splan/Main.java
@@ -9,6 +9,21 @@ import de.nils_witt.splan.gui.ConsoleGui;
 public class Main {
 
     public static void main(String[] args) {
-        ConsoleGui.launchGui();
+        boolean gui = true;
+
+        try {
+            if(System.getenv("GUI").equals("false")){
+                gui = false;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        if(gui){
+            ConsoleGui.launchGui();
+        }else {
+            CliApplication.main(args);
+        }
+
     }
 }
